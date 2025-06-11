847b5d480880e8f6062e247d8ef2a494106f7fb3
painfueg0 <kystax69@gmail.com>
2025-06-11 20:45:33 +0530

    follow up

--- src/client/database.ts
+++ src/client/database.ts
@@ -3,29 +3,61 @@
 import { randomUUID } from "node:crypto";
 
 import type { DatabaseManager } from "./databaseManager.js";
+import { Payload } from "../typings/types.js";
 
 export class Database<T> {
-  #path: string;
-  #manager: DatabaseManager;
+  path: string;
+  manager: DatabaseManager;
 
   constructor(manager: DatabaseManager, path: string) {
-    this.#path = path;
-    this.#manager = manager;
+    this.path = path;
+    this.manager = manager;
+  }
+
+  async #makeReq<D>(payload: Payload) {
+    if (this.manager.webSocket?.readyState !== WebSocket.OPEN)
+      throw new Error("Websocket connection has already been closed !");
+
+    const request = {} as {
+      promise: Promise<D | null>;
+      reject: (err?: Error) => void;
+      resolve: (args: D | null) => void;
+    };
+
+    request.promise = new Promise<D | null>((resolve, reject) => {
+      request.resolve = resolve;
+      request.reject = reject;
+    });
+
+    this.manager.requests.set(payload.requestId, request);
+    this.manager.webSocket!.send(JSON.stringify(payload));
+
+    setTimeout(() => {
+      if (!this.manager.requests.get(payload.requestId)) return;
+      request.reject(new Error("Request timed out"));
+      this.manager.requests.delete(payload.requestId);
+    }, 2500);
+
+    return request.promise;
   }
 
   async delete(key: string) {
-    return this.#manager.makeReq<null>({ requestId: randomUUID(), path: this.#path, method: "DELETE", key });
+    return this.#makeReq<null>({ requestId: randomUUID(), path: this.path, method: "DELETE", key });
   }
+
   async get(key: string) {
-    return this.#manager.makeReq<T | null>({ requestId: randomUUID(), path: this.#path, method: "GET", key });
+    return this.#makeReq<T | null>({ requestId: randomUUID(), path: this.path, method: "GET", key });
   }
+
   async all() {
-    return await this.#manager.makeReq<T | null>({ requestId: randomUUID(), path: this.#path, method: "ALL" });
+    return await this.#makeReq<T | null>({ requestId: randomUUID(), path: this.path, method: "ALL" });
   }
+
   async set(key: string, value: T) {
-    return this.#manager.makeReq<T>({ requestId: randomUUID(), path: this.#path, method: "SET", key, value: value });
+    return this.#makeReq<T>({ requestId: randomUUID(), path: this.path, method: "SET", key, value: value });
   }
+
   async has(key: string) {
-    return !!(await this.#manager.makeReq<T | null>({ requestId: randomUUID(), path: this.#path, method: "GET", key }));
+    return !!(await this.#makeReq<T | null>({ requestId: randomUUID(), path: this.path, method: "GET", key }));
   }
 }
--- src/client/databaseManager.ts
+++ src/client/databaseManager.ts
@@ -4,63 +4,32 @@ import { WebSocket } from "ws";
 import { EventEmitter } from "events";
 import { Database } from "./database.js";
 
-import type { ChildEvents, Payload, Response } from "../typings/types.js";
+import type { ChildEvents, Response } from "../typings/types.js";
 
 export class DatabaseManager extends EventEmitter<ChildEvents> {
-  #auth: string;
-  #requests = new Map();
-  #webSocket?: WebSocket;
-  #socketAddress: string;
+  auth: string;
+  requests = new Map();
+  webSocket?: WebSocket;
+  socketAddress: string;
 
   constructor(socketAddress: string, auth: string) {
     super();
-    this.#auth = auth;
-    this.#socketAddress = socketAddress;
+    this.auth = auth;
+    this.socketAddress = socketAddress;
   }
 
   async connect() {
-    this.#webSocket = new WebSocket(this.#socketAddress, { headers: { Authorization: this.#auth } });
+    this.webSocket = new WebSocket(this.socketAddress, { headers: { Authorization: this.auth } });
 
-    await new Promise((resolve) => this.#webSocket?.once("open", resolve));
+    await new Promise((resolve) => this.webSocket?.once("open", resolve));
 
-    this.#webSocket!.on("message", (message) => {
+    this.webSocket!.on("message", (message) => {
       const data = JSON.parse(message.toString()) as Response;
-      this.#requests.get(data.requestId)?.resolve(data.data);
-      this.#requests.delete(data.requestId);
+      this.requests.get(data.requestId)?.resolve(data.data);
+      this.requests.delete(data.requestId);
     });
-    this.#webSocket.on("error", (err) => this.emit("error", err));
-    this.#webSocket.once("close", () => this.emit("disconnected", this.#socketAddress));
-  }
-
-  /**
-   * @requires {@linkcode DatabaseManager#connect} to be called and awaited
-   * @throws if webSocket connection is not open i.e is closed or connecting or closing
-   */
-  async makeReq<D>(payload: Payload) {
-    if (this.#webSocket?.readyState !== WebSocket.OPEN)
-      throw new Error("Websocket connection has already been closed !");
-
-    const request = {} as {
-      promise: Promise<D | null>;
-      reject: (err?: Error) => void;
-      resolve: (args: D | null) => void;
-    };
-
-    request.promise = new Promise<D | null>((resolve, reject) => {
-      request.resolve = resolve;
-      request.reject = reject;
-    });
-
-    this.#requests.set(payload.requestId, request);
-    this.#webSocket!.send(JSON.stringify(payload));
-
-    setTimeout(() => {
-      if (!this.#requests.get(payload.requestId)) return;
-      request.reject(new Error("Request timed out"));
-      this.#requests.delete(payload.requestId);
-    }, 2500);
-
-    return request.promise;
+    this.webSocket.on("error", (err) => this.emit("error", err));
+    this.webSocket.once("close", () => this.emit("disconnected", this.socketAddress));
   }
 
   /**
@@ -69,7 +38,7 @@ export class DatabaseManager extends EventEmitter<ChildEvents> {
    * @throws if webSocket connection is not open i.e is closed or connecting or closing
    */
   createDatabase<T>(path: string): Database<T> {
-    if (this.#webSocket?.readyState !== WebSocket.OPEN)
+    if (this.webSocket?.readyState !== WebSocket.OPEN)
       throw new Error(`Please do "await <DatabaseManager>.connect()" before trying to create a database !`);
     return new Database<T>(this, path);
   }
