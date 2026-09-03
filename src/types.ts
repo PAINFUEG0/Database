f601dbc5fb9a6f0917f2b7b3570b993823aa8e34
painfuego <kystax69@gmail.com>
2026-09-03 21:01:04 +0530

    Add types for Payload and implement WebSocket message handling

---
+++ src/types.ts
@@ -0,0 +1,16 @@
+/** @format */
+
+export type Prettify<T> = { [K in keyof T]: T[K] } & {};
+
+export type BasePayload = { path: string; requestId: string };
+
+export type Payload = Prettify<
+  BasePayload &
+    (
+      | { method: "ALL" }
+      | { method: "SET"; key: string; value: any }
+      | { method: "GET" | "DELETE" | "HAS"; key: string }
+      | { method: "GET_MANY" | "DELETE_MANY"; keys: string[] }
+      | { method: "SET_MANY"; data: { key: string; value: any }[] }
+    )
+>;
--- src/ws.ts
+++ src/ws.ts
@@ -1,7 +1,37 @@
 /** @format */
 
+import { resolve } from "path";
+import { Database } from "./database";
+
+import type { Payload } from "./types";
 import type { WebSocket, RawData } from "ws";
 
-export function handleIncomingWebsocketMessage(this: WebSocket, data: RawData, isBinary: boolean) {
-  console.log({ isBinary, data: data.toString() });
+const databases = new Map<string, Database<any>>();
+
+export function handleIncomingWebsocketMessage(this: WebSocket, data: RawData) {
+  const PL: Payload = JSON.parse(data.toString());
+
+  let { path, requestId } = PL;
+  path = resolve("./", "storage", path);
+
+  const db = databases.get(path) || databases.set(PL.path, new Database(path)).get(path)!;
+
+  try {
+    //@ts-expect-error loose typings
+    const data = handlers[PL.method](db, PL);
+    this.send(JSON.stringify({ requestId, data }));
+  } catch (error) {
+    this.send(JSON.stringify({ requestId, error }));
+  }
 }
+
+const handlers = {
+  ALL: (db: Database<any>) => db.all(),
+  HAS: (db: Database<any>, PL: Payload & { method: "HAS" }) => db.has(PL.key),
+  GET: (db: Database<any>, PL: Payload & { method: "GET" }) => db.get(PL.key),
+  DELETE: (db: Database<any>, PL: Payload & { method: "DELETE" }) => db.delete(PL.key),
+  SET: (db: Database<any>, PL: Payload & { method: "SET" }) => db.set(PL.key, PL.value),
+  GET_MANY: (db: Database<any>, PL: Payload & { method: "GET_MANY" }) => db.getMany(PL.keys),
+  SET_MANY: (db: Database<any>, PL: Payload & { method: "SET_MANY" }) => db.setMany(PL.data),
+  DELETE_MANY: (db: Database<any>, PL: Payload & { method: "DELETE_MANY" }) => db.deleteMany(PL.keys)
+};
