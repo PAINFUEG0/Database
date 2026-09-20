d030a3e805ee64158a3c20bf684e5deef6bb15dd
painfuego <kystax69@gmail.com>
2026-09-20 11:07:04 +0530

    follow up

--- src/server/handleWebsocketMessages.ts
+++ src/server/handleWebsocketMessages.ts
@@ -1,8 +1,7 @@
 /** @format */
 
-import { resolve } from "path";
+import { actions } from "./actions.js";
 import { databases } from "./databaseServer.js";
-import { KeyValueStore } from "./keyValueStore.js";
 
 import type { Payload } from "../types.js";
 import type { WebSocket, RawData } from "ws";
@@ -15,24 +14,10 @@ export async function handleIncomingWebsocketMessages(this: WebSocket, raw: RawD
 
   try {
     // @ts-expect-error loose typings
-    const data = await handlers[PL.method](db, PL);
+    const data = await actions[PL.method](db, PL);
     this.send(JSON.stringify({ requestId, data }));
   } catch (error) {
     error = error instanceof Error ? error.message : error;
     this.send(JSON.stringify({ requestId, error }));
   }
 }
-
-const handlers = {
-  INIT: async (_: any, pl: Payload & { method: "INIT" }) =>
-    databases.set(pl.path, await new KeyValueStore({ path: resolve("./", "storage", pl.path), ...pl.options }).init()),
-
-  ALL: (db: KeyValueStore<any>) => db.all(),
-  HAS: (db: KeyValueStore<any>, PL: Payload & { method: "HAS" }) => db.has(PL.key),
-  GET: (db: KeyValueStore<any>, PL: Payload & { method: "GET" }) => db.get(PL.key),
-  DELETE: (db: KeyValueStore<any>, PL: Payload & { method: "DELETE" }) => db.delete(PL.key),
-  SET: (db: KeyValueStore<any>, PL: Payload & { method: "SET" }) => db.set(PL.key, PL.value),
-  GET_MANY: (db: KeyValueStore<any>, PL: Payload & { method: "GET_MANY" }) => db.getMany(PL.keys),
-  SET_MANY: (db: KeyValueStore<any>, PL: Payload & { method: "SET_MANY" }) => db.setMany(PL.data),
-  DELETE_MANY: (db: KeyValueStore<any>, PL: Payload & { method: "DELETE_MANY" }) => db.deleteMany(PL.keys)
-};
