fc2ae7317719cea20f9fe805117e74436648986d
painfuego <kystax69@gmail.com>
2026-09-26 13:40:17 +0530

    fix: remove console error logging from handleRestRequests and handleIncomingWebsocketMessages

--- src/server/handleWebsocketMessages.ts
+++ src/server/handleWebsocketMessages.ts
@@ -17,7 +17,6 @@ export async function handleIncomingWebsocketMessages(this: WebSocket, raw: RawD
     const data = await actions[PL.method](db, PL);
     this.send(JSON.stringify({ requestId, data }));
   } catch (error) {
-    console.error(error);
     error = error instanceof Error ? error.message : error;
     this.send(JSON.stringify({ requestId, error }));
   }
